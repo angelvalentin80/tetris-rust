665060ce9f25b75b8ed75e55f8eb835463a34905
Angel V <ajrocketmail@gmail.com>
2025-03-28 18:48:33 -0400

    added gravity speed changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use bevy::prelude::*;
 use std::collections::VecDeque;
 use crate::grid::{draw_grid, Grid, GridConfig, GRID_CELL_SIZE, GRID_WIDTH, GRID_HEIGHT, CELL_BORDER_WIDTH, RedrawGridEvent, redraw_grid, CheckForLinesEvent, check_for_lines, reset_grid};
-use crate::tetromino::{draw_tetromino, move_tetromino, detect_lock_position, spawn_tetromino, gravity, SpawnTetrominoEvent, draw_ghost_piece, RedrawGhostCellsEvent, LockInTetrominoEvent, draw_next_piece_text, draw_next_piece, spawn_next_piece, SpawnNextPieceEvent, despawn_active_tetromino, despawn_next_piece};
+use crate::tetromino::{draw_tetromino, move_tetromino, detect_lock_position, spawn_tetromino, gravity, SpawnTetrominoEvent, draw_ghost_piece, RedrawGhostCellsEvent, LockInTetrominoEvent, draw_next_piece_text, draw_next_piece, spawn_next_piece, SpawnNextPieceEvent, despawn_active_tetromino, despawn_next_piece, gravity_seconds_for_level, update_gravity_timer};
 use crate::systems::lock_in_tetromino;
 use crate::resources::{GravityTimer, LockInTimer, TetrominoQueue, GameState};
 use crate::queue::{shuffle_tetrominoes_into_queue, detect_bag_low, BagLowEvent, restart_queue};
 use crate::game_manager::{GameStartEvent, detect_start_game, detect_restart_game, GameRestartEvent};
-use crate::scoring::{Scoring, draw_level_and_score, RedrawLevelAndScoreEvent, reset_level_and_score};
+use crate::scoring::{Scoring, draw_level_and_score, RedrawLevelAndScoreEvent, reset_level_and_score, LevelUpEvent};
 
 mod grid;
 mod tetromino;
@@ -30,6 +30,7 @@ fn main() {
         .add_event::<SpawnNextPieceEvent>()
         .add_event::<GameRestartEvent>()
         .add_event::<RedrawLevelAndScoreEvent>()
+        .add_event::<LevelUpEvent>()
         // Systems
         .add_systems(Startup, 
             (
@@ -39,7 +40,7 @@ fn main() {
         .add_systems(Update, 
             (
                 (detect_start_game, redraw_grid, shuffle_tetrominoes_into_queue, spawn_tetromino, draw_tetromino, draw_ghost_piece, draw_next_piece_text, draw_level_and_score, spawn_next_piece, draw_next_piece).chain(),
-                (gravity, lock_in_tetromino, move_tetromino, detect_lock_position, detect_bag_low, check_for_lines),
+                (gravity, lock_in_tetromino, move_tetromino, detect_lock_position, detect_bag_low, check_for_lines, update_gravity_timer),
                 (restart_queue, detect_restart_game, despawn_active_tetromino, reset_grid, despawn_next_piece, reset_level_and_score)
             ))
         .run();
@@ -60,7 +61,8 @@ fn setup(mut commands: Commands) {
     commands.insert_resource(Grid::new());
 
     // Add our gravity resource 
-    let gravity_timer = GravityTimer(Timer::from_seconds(1.0, TimerMode::Repeating));
+    let initial_time = gravity_seconds_for_level(1);
+    let gravity_timer = GravityTimer(Timer::from_seconds(initial_time, TimerMode::Repeating));
     commands.insert_resource(gravity_timer);
 
     // Add our lock in resource
